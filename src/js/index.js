7447ac4c25da95fc7f227b5297c47b932f2460ee
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2025-07-22 07:40:13 +0900

    Registrando o eventListinner 'FocoFinalizado'

--- src/js/index.js
+++ src/js/index.js
@@ -77,7 +77,11 @@ const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0 ){
         beep.play()
         alert('Tempo Finalizado')
-        
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if(focoAtivo){
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
         zerar()
         return
     }
--- src/js/script-crud.js
+++ src/js/script-crud.js
@@ -7,6 +7,7 @@ const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-ta
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 let tarefaSelecionada = null 
+let liTarefaSelecionada = null
 
 const limparFormulário = () => {
     textarea.valeu = "";
@@ -68,9 +69,11 @@ function criarElementoTarefa(tarefa) {
         if(tarefaSelecionada == tarefa) {
             paragrafoDescricaoTarefa.textContent = ''
             tarefaSelecionada = null
+            liTarefaSelecionada = null
             return
         }
         tarefaSelecionada = tarefa
+        liTarefaSelecionada = li
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
 
         li.classList.add("app__section-task-list-item-active")
@@ -100,3 +103,11 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 });
+
+document.addEventListener('FocoFinalizado', () => {
+    if(tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove("app__section-task-list-item-active")
+        liTarefaSelecionada.classList.add("app__section-task-list-item-complete")
+        liTarefaSelecionada.querySelector("button").setAttribute("disabled", "disabled")
+    }
+})
